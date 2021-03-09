9dd237652dfb2fc4942d86aa2a40593b94b7389b
Bonnie3206 <molk3206@gmail.com>
2021-03-09 16:46:57 +0800

    new0309

---
+++ Hw01_00753143_test2/ContentView.swift
@@ -0,0 +1,117 @@
+//
+//  ContentView.swift
+//  Hw01_00753143_test2
+//
+//  Created by CK on 2021/3/7.
+//
+import SwiftUI
+
+struct ContentView: View {
+    @State private var AnswearShow = false
+    @State private var number = 0
+    @State private var showingAlert = false
+    var body: some View {
+        
+        ZStack{
+            Image("猜燈謎背景3")
+                .resizable()
+                .opacity(0.6)
+            VStack(alignment: .leading,spacing :70){
+                
+                HStack{
+                    Text("第" + String(number+1) + "題")
+                        .font(.title).foregroundColor(.white).multilineTextAlignment(.center).padding(16)
+                        Spacer()
+                }.background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+                
+                Text(Question[Sequence[number]]).font(.title).fontWeight(.bold).padding()
+                
+                HStack{
+                    
+                    
+                    if AnswearShow == true {
+                        Text(Answer[Sequence[number]])
+                            .font(.largeTitle)
+                            .multilineTextAlignment(.trailing)
+                            .padding()
+                           
+                    }else{
+                        Text("答案是...?")
+                            .fontWeight(.heavy)
+                            .multilineTextAlignment(.leading)
+                            .padding()
+                            
+                    }
+                    
+                }
+                
+                Image("熊玩手機")
+                .resizable()
+                .scaledToFit()
+              
+                HStack{
+                    
+                    Button(action: {
+                        AnswearShow = true
+                    },label: {
+                        Text("看答案")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .background(Color.pink)
+                            .cornerRadius(20.0)
+                    
+                    }).padding(20)
+                    
+                    Button(action: {
+                      
+                        if(number > 8){
+                            showingAlert = true
+                            Sequence.shuffle()
+                            number = 0
+                        }else{
+                            number = number + 1
+                            AnswearShow = false
+                        }
+                      
+                    },label: {
+                        
+                        
+                        Text("下一題")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .background(Color.yellow)
+                            .cornerRadius(20.0)
+                            
+                    }).padding(1.0)
+                    
+                    Button(action: {
+                        Sequence.shuffle()
+                        number = 0
+                        AnswearShow = false
+                    },label: {
+                        Text("重選10題")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .background(Color.yellow)
+                            .cornerRadius(10.0)
+                    }).alert(isPresented: $showingAlert) { () -> Alert in
+                        let answer = "已經玩超過10題了 重新玩10題吧～"
+                        return Alert(title: Text(answer))
+                    }.padding(30)
+                }
+                
+            }.onAppear(perform: {
+                Sequence.shuffle()
+            })
+        }
+        
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hw01_00753143_test2/Hw01_00753143_test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Hw01_00753143_test2App.swift
+//  Hw01_00753143_test2
+//
+//  Created by CK on 2021/3/7.
+//
+
+import SwiftUI
+
+@main
+struct Hw01_00753143_test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
